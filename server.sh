7c6f39fad10f7e56e71c9cb6d90e1485bf506f05
j-romchain <57956651+j-romchain@users.noreply.github.com>
2022-06-19 20:39:45 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,4 @@
 curl -sSL https://github.com/jpillora/chisel/releases/download/v1.7.6/chisel_1.7.6_linux_amd64.gz | zcat > /bin/chisel
 chmod +x /bin/chisel
+dig +short txt ch whoami.cloudflare @1.0.0.1
 chisel server --auth user:pass --socks5 --reverse
