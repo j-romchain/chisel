2faf3fcaa856d084634947307dc84c8804f8ff67
j-romchain <57956651+j-romchain@users.noreply.github.com>
2023-12-10 20:21:04 -0500

    Update to 177

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
-curl -sSL https://github.com/jpillora/chisel/releases/download/v1.7.6/chisel_1.7.6_linux_amd64.gz | zcat > /bin/chisel
+curl -sSL https://github.com/jpillora/chisel/releases/download/v1.7.7/chisel_1.7.7_linux_amd64.gz | zcat > /bin/chisel
 chmod +x /bin/chisel
 chisel server --auth user:pass --socks5 --reverse
